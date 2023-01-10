427badc77b6da3aa5f0ad6dc0f785c565c210e89
Justin Monteza <26825560+jmonteza@users.noreply.github.com>
2023-01-09 18:23:32 -0700

    Update find_version.py

--- find_version.py
+++ find_version.py
@@ -3,3 +3,8 @@ import requests
 print(requests.__version__)
 
 print(requests.get("https://www.google.com").text)
+
+GITHUB_URL = "https://raw.githubusercontent.com/jmonteza/CMPUT404-LAB1/master/find_version.py"
+
+print(requests.get(GITHUB_URL).text)
+
